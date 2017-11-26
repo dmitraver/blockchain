6ca65e1080edbf78a636bb4a73dac5780f3df0b3
Thomas Deniffel <t.deniffel@skytala-gmbh.com>
2017-11-26 21:27:16 +0100

    Block + Transaction complete

--- src/main/java/com/skytala/blockchain/Block.java
+++ src/main/java/com/skytala/blockchain/Block.java
@@ -1,7 +1,90 @@
 package com.skytala.blockchain;
 
+import java.util.List;
+
 /**
- * Created by thomas on 26.11.17.
+ * The basic building block of the blockchain
+ * Each block can contain many transactions (depends on the blockchain implementation)
+ * Each block can have an fixed size (if you want. In Bitcoin this is the case)
+ * This blocks are the append-only list of the blockchain
  */
 public class Block {
+    private Integer index;   // For the global index normally an auto-increment
+    private Long timestamp;  // in Unix time
+    private List<Transaction> transactions; // the actual content of the blockchain
+    private Integer proof;   // That the block actually fits in the chain
+    /**
+     * Each new block contains within itself, the hash of the previous Block.
+     * This is crucial because it’s what gives blockchains immutability:
+     * If an attacker corrupted an earlier Block in the chain then all subsequent
+     * blocks will contain incorrect hashes.
+     */
+    private String previousHash;
+
+
+    public Integer getIndex() {
+        return index;
+    }
+
+    public void setIndex(Integer index) {
+        this.index = index;
+    }
+
+    public Long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(List<Transaction> transactions) {
+        this.transactions = transactions;
+    }
+
+    public Integer getProof() {
+        return proof;
+    }
+
+    public void setProof(Integer proof) {
+        this.proof = proof;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Block block = (Block) o;
+
+        return index != null ? index.equals(block.index) : block.index == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return index != null ? index.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Block{" +
+                "index=" + index +
+                ", timestamp=" + timestamp +
+                ", transactions=" + transactions +
+                ", proof=" + proof +
+                ", previousHash='" + previousHash + '\'' +
+                '}';
+    }
 }
--- src/main/java/com/skytala/blockchain/Transaction.java
+++ src/main/java/com/skytala/blockchain/Transaction.java
@@ -1,4 +1,74 @@
 package com.skytala.blockchain;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.NotNull;
+
+/**
+ * One entry in the list of transaction within a block
+ * If a new block is minded, the transaction is part of the block and
+ * the block is part in the longest chain (and therefore outlives the
+ * consensus algorithm) the transaction is committed, otherwise rolled
+ * back.
+ *
+ * It can take a while, until you know, that your transaction was part
+ * of the longest chain.
+ * Because you want be able to add your transaction to the last block
+ * and to the longest chain you know, you don't want, that it is too
+ * easy to mine block. Because every time a new block is mined it can
+ * be, that your transaction is rolled back.
+ */
 public class Transaction {
+    @NotNull private String sender;
+    @NotNull private String recipient;
+    @NotNull private Integer amount;
+
+    public Transaction(@JsonProperty("sender") String sender,
+                       @JsonProperty("recipient") String recipient,
+                       @JsonProperty("amount") Integer amount) {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (sender != null ? !sender.equals(that.sender) : that.sender != null) return false;
+        if (recipient != null ? !recipient.equals(that.recipient) : that.recipient != null) return false;
+        return amount != null ? amount.equals(that.amount) : that.amount == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = sender != null ? sender.hashCode() : 0;
+        result = 31 * result + (recipient != null ? recipient.hashCode() : 0);
+        result = 31 * result + (amount != null ? amount.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Transaction{" +
+                "sender='" + sender + '\'' +
+                ", recipient='" + recipient + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
 }
